0d1d091c9434f08240e64571f744fa8c480f0d5a
Hasan Siam <hasansiam4545@gmail.com>
2021-02-27 19:53:41 +0600

    updated app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div style={{backgroundColor: 'black', padding: '10px 30px', textAlign: 'center'}}>
+
+            <p style={{color:'white'}}>All Rights Reserved By <strong> <a style={{textDecoration: 'none', color: 'skyblue'}} href="https://m.facebook.com/obujmon.obujmon.16" target='_blank'>Hasan Siam</a></strong></p>
+            
+        </div>
+    );
+}
+
+export default Footer;
